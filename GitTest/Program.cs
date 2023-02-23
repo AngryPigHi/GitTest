65b83d76adbd4a1be05ac710835c81943178fd7c
Blithe Zhu <2375512561@qq.com>
2023-02-23 13:50:58 +0800

    2023年2月23日13:50:56

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,19 +6,23 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World1!");
-            Console.WriteLine("Hello World2!");
+            Console.WriteLine("Hello World1--11!");
+            Console.WriteLine("Hello World2--22!");
+            Console.WriteLine("Hello World3--33!");
             Console.WriteLine("From 3 Zhuxb");
-            Console.WriteLine("Hello morning!");
+            Console.WriteLine("Hello morning--44!");
             Console.WriteLine("From Zhuxb");
             Console.WriteLine("Hello World11!");
             Console.WriteLine("From Zhuxb11");
             Console.WriteLine("Hello World2222222222222222222!");
             Console.WriteLine("Hello World2222222222222222222!");
-            Console.WriteLine("From Zhuxb222222222222222222222");
+            Console.WriteLine("Hello World1!");
+            Console.WriteLine("From Zhu1112222222");
             Console.WriteLine("1");
             Console.WriteLine("2");
+            Console.ReadKey();
             Console.WriteLine("3");
+            Console.WriteLine("4");
         }
     }
 }
